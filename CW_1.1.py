f0095affadd86da575c48b14f9b66387e4e7f87b
Fallen-Codez <161318574+Fallen-Codez@users.noreply.github.com>
2024-03-08 02:24:10 +0000

    Add files via upload
    CW_1.1

---
+++ CW_1.1.py
@@ -0,0 +1,47 @@
+import time
+
+
+def is_palindrome(n):
+    return str(n) == str(n)[::-1]
+
+
+def is_prime(n):
+    if n <= 1:
+        return False
+    if n == 2 or n == 3:
+        return True
+    if n % 2 == 0:
+        return False
+    for _ in range(3, int(n ** 0.5) + 1, 2):
+        if n % i == 0:
+            return False
+    return True
+
+
+var_m = int(input("Enter the small number: "))
+var_n = int(input("Enter the large number: "))
+
+pali_list = []
+start = time.time()
+
+if var_n <= 2:
+    pali_list.append(2)
+    var_n = 3
+
+if var_n % 2 == 0:
+    var_n = var_n + 1
+
+for i in range(var_m, var_n + 1, 2):
+    if is_palindrome(i) and is_prime(i):
+        pali_list.append(i)
+
+end = time.time()
+
+print("Total time taken: ", end - start)
+print("Total special number: ", len(pali_list))
+
+if len(pali_list) < 6:
+    print(pali_list)
+else:
+    print(f"First three Palindromic Prime numbers: {pali_list[:3]}")
+    print(f"Last three Palindromic Prime numbers: {pali_list[-3:]}")
